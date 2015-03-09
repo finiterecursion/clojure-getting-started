4f243b0e0cae9e7e9356a4ea15d010a849e37da6
Arun S <arun.finiterecursion@gmail.com>
2015-03-09 10:26:37 +0530

    Indents hellofile

--- hellofile.clj
+++ hellofile.clj
@@ -1,14 +1,8 @@
 (defn foo
   "I don't do a whole lot."
   [args]
-  (spit "hello.txt" (with-out-str (println
-				  			"Filename: " 
-				  			(first args) 
-				  			"\nArguments: " 
-				  			(rest args) 
-				  			"\nHello, World!"
-  						))
-  )
- )
+  (spit "hello.txt" 
+  	(with-out-str 
+  	  (println "Filename: " (first args) "\nArguments: " (rest args)  "\nHello, World!"))))
 
 (foo *command-line-args*)
